5448da10ab3fc0ff7d3da0a0c16a97fedea2c6ea
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-23 02:48:42 +0600

    add a callback function in App

--- server/index.js
+++ server/index.js
@@ -8,4 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const server = app.listen()
+const server = app.listen(process.env.PORT,()=>{
+ console.log(`Server started on port ${process.env.PORT}`)
+})
