f7419624b3699c15120f7b7585d58116b1c8a565
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-23 03:52:19 +0600

    add Whatsapp image in login

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
 import React from "react";
 
 function login() {
-  return <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6">login</div>;
+  return <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6">
+    <div className="flex items-center justify-center gap-2 text-white">
+      <Image 
+      src="/whatsapp.gif" alt="Whatsapp" 
+      height={300} width={300}
+      />
+    </div>
+  </div>;
 }
 
 export default login;
