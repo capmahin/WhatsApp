af28aa1064757a71263a249407d5e47fc23b14ed
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-24 01:25:20 +0600

    if there no email

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,8 +1,9 @@
-
-
 export const checkUser = (req,res,next)=>{
     try {
-        const {email} = req.body
+        const {email} = req.body;
+        if(!email){
+            return res.json({msg: "Email is required.", status:false})
+        }
     } catch (error) {
         next(error)
     }
