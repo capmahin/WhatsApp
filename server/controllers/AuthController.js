d98f924a576b6a80ad4a1987401203805feb15c4
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-24 01:26:11 +0600

    if there email then

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,9 +1,12 @@
+import getPrismaInstance from "../utils/PrismaClient";
+
 export const checkUser = (req,res,next)=>{
     try {
         const {email} = req.body;
         if(!email){
             return res.json({msg: "Email is required.", status:false})
         }
+        const prisma = getPrismaInstance();
     } catch (error) {
         next(error)
     }
