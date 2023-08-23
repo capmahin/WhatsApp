6c5313aa0c15444cf4903d3c95c5cdf5def283e0
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-24 04:19:24 +0600

    add classes in onboarding

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function onboarding() {
-  return <div className="bg-panel-header-background">onboarding</div>;
+  return <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center">onboarding</div>;
 }
 
 export default onboarding;
