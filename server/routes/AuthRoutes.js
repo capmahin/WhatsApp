f56a3816de55c988f3186a7f0d36a8c22368c4ec
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-24 01:36:16 +0600

    export

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,6 +1,8 @@
 import {Router} from "express";
-import { checkUser } from "../controllers/AuthController";
+import { checkUser } from "../controllers/AuthController.js";
 
 const router = Router()
 
 router.post("/check-user", checkUser);
+
+export default router;
