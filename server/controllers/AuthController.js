f2bdbe33059d583c602457e50611db1b566ccc3e
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-24 01:28:50 +0600

    add findUnick

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,12 +1,13 @@
 import getPrismaInstance from "../utils/PrismaClient";
 
-export const checkUser = (req,res,next)=>{
+export const checkUser = async (req,res,next)=>{
     try {
         const {email} = req.body;
         if(!email){
             return res.json({msg: "Email is required.", status:false})
         }
         const prisma = getPrismaInstance();
+        const user = await prisma.user.findUnique({where:{email}});
     } catch (error) {
         next(error)
     }
