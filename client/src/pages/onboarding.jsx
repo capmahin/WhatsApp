d85c5b65dc8f451c811bf8b984a4d4f4fe885380
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-25 01:43:55 +0600

    add new div

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -12,7 +12,12 @@ function onboarding() {
     />
     <span className="text-7xl">Whatsapp</span>
     </div>
-    <h2 className="text-7xl">Create your profile</h2>
+    <h2 className="text-2xl">Create your profile</h2>
+    <div className="flex gap-6 mt-6">
+      <div className="flex flex-col items-center justify-center mt-5 gap-6">
+
+      </div>
+    </div>
   </div>;
 }
 
